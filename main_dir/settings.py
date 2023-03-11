cfff323f346d0b84503e5d29c489b6f7f86665ed
siddharth-kt <51018644+siddharth-kt@users.noreply.github.com>
2023-03-11 14:34:28 +0530

    Update settings.py

--- main_dir/settings.py
+++ main_dir/settings.py
@@ -75,13 +75,6 @@ WSGI_APPLICATION = 'main_dir.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': Path(__file__).resolve().parent.parent.parent / 'db.sqlite3',   # edited
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
